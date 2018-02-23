57adbc676a99f53427a2aa2d90c7654fe590caee
Jason West <jason@Jasons-MacBook-Pro.local>
2018-02-23 14:53:56 -0500

    Optimized a lot, wrote data to file, drew another bar graph for distance

--- Mini Project 1/nerd_data.py
+++ Mini Project 1/nerd_data.py
@@ -11,7 +11,6 @@ ws = wb.active
 # Global Variables
 questionDistributions = {}
 questionMeanVarEnt = {}
-questionTitles = []
 
 ''' Parses each column using openpyxl, then computes the
     and returns the response distribution of the specific 
@@ -24,8 +23,7 @@ def distributionOfColumn(columnLabel):
     for row in range(2, ws.max_row + 1):
         for column in columnLabel:
             cellName = "{}{}".format(column, row)
-            if ws[cellName].value != 0:                             # omit the 0s from the data set
-                responses.append(ws[cellName].value)  
+            responses.append(ws[cellName].value)  
 
     # populate list with rates per response
     for i in range(5):
@@ -33,7 +31,8 @@ def distributionOfColumn(columnLabel):
     
     return np.divide(questionResponses, len(responses))
 
-''' Computes and returns the mean, variance, and entropy 
+
+''' Computes and returns the mean, variance, and entropy  
     for a specific question based on the response distributions. 
     Makes use of numpy for the list arithmetic. '''
 def computeMeanVarEnt(response, probs):
@@ -44,73 +43,86 @@ def computeMeanVarEnt(response, probs):
     entropy = sum(np.multiply(negProb, np.log2(probs)))
     return expectation, variance, entropy
 
+
 ''' Computes the Hellinger distance of each possible question pair. 
     Returns a dictionary of the values with an accompanying key 
     describing the two questions.'''
-def calculateHellinger(questProbs, titles):
+def calculateHellinger(distributions, questionTitle):
     hellinDict = {}
     divSqRtTwo = 1 / math.sqrt(2)
     for qA in range(26):
         for qB in range(qA + 1, 26):
-            probA = questProbs.get(titles[qA])
-            probB = questProbs.get(titles[qB])
-            temp = np.sqrt(sum(np.square(np.subtract(np.sqrt(probA), np.sqrt(probB)))))
+            probabilityOfA = distributions.get(questionTitle[qA])
+            probabilityOfB = distributions.get(questionTitle[qB])
+            temp = np.sqrt(sum(np.square(np.subtract(np.sqrt(probabilityOfA), np.sqrt(probabilityOfB)))))
             hellin = np.multiply(np.sqrt(temp), divSqRtTwo)
 
+            # format key and create dictionary
             hellinKey = "%d, %d" % (qA + 1, qB + 1)
             hellinDict.update({hellinKey:hellin})
     return hellinDict
 
 
-def createBarGraph(titles, distributions):
-    nGroups = len(titles)
+''' Draws the bar graph for all the distributions per response
+    for each question.'''
+def distributionBarGraph(distributions, questionTitle):
     colors = ['g', 'b', 'r', 'y', 'k']
 
     # bar graph setup
-    plt.figure(figsize=(18, 3))
-    index = np.arange(nGroups)
+    plt.figure(figsize=(13, 4))
+    index = np.arange(len(distributions.keys()))
     barWidth = 0.15
-    opacity = 0.8
 
     # creates graph data
     for rating in range(5):
-        q = []
         counter = barWidth * (rating + 1)
-        for keys in distributions:
-            q.append(distributions.get(keys)[rating])
-        plt.bar(index + counter, tuple(q), barWidth, alpha=opacity, 
-                color=colors[rating], label=(rating + 1))   
+        probabilityOfResponse = tuple([probs[rating] for probs in distributions.values()])
+        plt.bar(index + counter, probabilityOfResponse, barWidth, color=colors[rating], label=(rating + 1))   
 
     plt.xlabel('Questions')
     plt.ylabel('Distributions')
-    plt.xticks(index + (barWidth * 3), tuple(titles))
+    plt.xticks(index + (barWidth * 3), tuple(questionTitle))
     plt.legend()
+    plt.show()   
+
+
+''' Draws the bar graph for the hellinger distance spreads for 
+    each pair of question. '''
+def hellingerBarGraph(distances):
+    plt.figure(figsize=(10, 5))
+    index = np.arange(len(distances.keys()))
+    barWidth = 0.75
+
+    plt.bar(index, tuple(distances.values()), barWidth, color='b')
+    plt.xlabel('Question Pairs')
+    plt.ylabel('Hellinger Distances')
+    plt.tick_params(axis='x', which='both', bottom='off', labelbottom='off')
     plt.show()
 
 
-def createScatterPlot(hellinDict):
-    x_axis = []
-    for i in range(325):
-        x_axis.append(i + 1)
+''' Writes the dictionary data passed to a .txt file in a 
+    readable format. '''
+def writeDataToFile(filename, data, dictValueNum):
+    type = ['mean', 'variance', 'entropy']
+    f = open(filename, "w")
+    for key, value in data.items():
+        if dictValueNum <= 1:
+            f.write("%s: %s\n" % (key, round(value, 4)))
+        else:
+            f.write("%s\n" % key)
+            for i in range(dictValueNum):
+                if dictValueNum == 3:
+                    f.write("  %s: %s\n" % (type[i], round(value[i], 3)))
+                else:
+                    f.write("  %d: %s\n" % (i+1, round(value[i], 3)))  
+    f.close()
 
-    colors = (0, 0, 0)
-    area = np.pi*3
- 
-    # Plot
-    plt.scatter(x_axis, hellinDict.values(), s=area, c=colors, alpha=0.5)
-    plt.title('Hellinger\'s Distance of Question Pairs')
-    plt.xlabel('Pairs of Questions')
-    plt.ylabel('Distance')
-    plt.show()         
-
-# Formats the data using the functions above and enters it into a dictionary for easy access. 
-# will also create a dictionary that holds all the question totals
+# Main
 answer = list(range(1, 6))
 
 for i in range(26):
     colLetter = chr(65 + i)
     questionName = "Q%d" % (i + 1)
-    questionTitles.append(questionName)
     
     questionProb = distributionOfColumn(colLetter)
     questionMean, questionVar, questionEnt = computeMeanVarEnt(answer, questionProb)
@@ -118,4 +130,15 @@ for i in range(26):
     questionMeanVarEnt.update({questionName:[questionMean, questionVar, questionEnt]})          
     questionDistributions.update({questionName:questionProb})           
 
-hellDict = calculateHellinger(questionDistributions, questionTitles)
+keys = [key for key in questionDistributions.keys()]
+distances = calculateHellinger(questionDistributions, keys)
+distanceMaxKey = max(distances, key=distances.get)
+distanceMinKey = min(distances, key=distances.get)
+
+writeDataToFile("question_distributions.txt", questionDistributions, 5)
+writeDataToFile("question_mean_var_ent.txt", questionMeanVarEnt, 3)
+writeDataToFile("question_hellinger.txt", distances, 1)
+distributionBarGraph(questionDistributions, keys)
+hellingerBarGraph(distances)
+print("%s: %s" % (distanceMaxKey, round(distances.get(distanceMaxKey), 3)))
+print("%s: %s" % (distanceMinKey, round(distances.get(distanceMinKey), 3)))
